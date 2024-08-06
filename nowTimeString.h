025da4b2ecd3f8ef54c622b99fef60d95de77a36
peter@dell-potato <pieeaterpeter69@gmail.com>
2024-08-07 01:45:50 +0800

    notTimeString set default format

--- nowTimeString.h
+++ nowTimeString.h
@@ -4,7 +4,7 @@
 #include <chrono>
 #include <ctime>
 
-std::string getCurrentTimeString()
+std::string getCurrentTimeString(const char* format="%Y-%m-%d %H:%M:%S")
 {
     auto now = std::chrono::system_clock::now();
     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
@@ -12,7 +12,7 @@ std::string getCurrentTimeString()
     std::tm now_tm = *std::localtime(&now_c);
 
     std::ostringstream timeStream;
-    timeStream << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S");
+    timeStream << std::put_time(&now_tm, format);
 
     return timeStream.str();
 }
